ba62511b83ca2d855ef69ffb05599bcebf2b4355
VTPartner <vtpartner7@gmail.com>
2024-08-25 13:58:43 +0530

    testing

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.get('/api/', async (req, res) => {
   try {
     const result = await pool.query('SELECT NOW()');
     console.log(`Result::${result.row[0]}`);
+    
     res.send(result.rows[0]);
   } catch (error) {
     res.status(500).send('Database query error');
