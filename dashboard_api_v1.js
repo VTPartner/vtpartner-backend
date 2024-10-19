eb19d89dcd7901de2c6e7389c4e3abbb370a7ab2
VTPartner <vtpartner7@gmail.com>
2024-10-19 17:27:55 +0530

    s

--- dashboard_api_v1.js
+++ dashboard_api_v1.js
@@ -617,6 +617,7 @@ router.post("/add_vehicle_price", verifyToken, async (req, res) => {
 
     // If there are missing fields, return an error response
     if (missingFields) {
+      console.log(`Missing required fields: ${missingFields.join(", ")}`);
       return res.status(400).send({
         message: `Missing required fields: ${missingFields.join(", ")}`,
       });
