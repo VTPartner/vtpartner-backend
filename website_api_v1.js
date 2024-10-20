4a7151ee3a755dd62a7991025fab99733b11cd10
VTPartner <vtpartner7@gmail.com>
2024-10-20 21:22:51 +0530

    s

--- website_api_v1.js
+++ website_api_v1.js
@@ -72,7 +72,8 @@ router.get("/distance", async (req, res) => {
     const response = await axios.get(
       `https://maps.googleapis.com/maps/api/distancematrix/json?origins=place_id:${origins}&destinations=place_id:${destinations}&units=metric&key=${apiKey}`
     );
-    res.json(response.data);
+    // res.json(response.data);
+    res.status(200).json(response.data);
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Error fetching distance data" });
