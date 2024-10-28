b9e11f68a1b52aa30956d42a24fe9ae6fc40a370
VTPartner <vtpartner7@gmail.com>
2024-10-28 10:12:11 +0530

    s

--- dashboard_api_v1.js
+++ dashboard_api_v1.js
@@ -1879,7 +1879,7 @@ router.post("/register_agent", verifyToken, async (req, res) => {
         ) 
         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, 
           $10, $11, $12, $13, $14, $15, $16, $17, $18, 
-          $19, $20, $21, $22, $23, $24)
+          $19, $20, $21, $22, $23, $24,$25)
         RETURNING ${driverIdField}
       `;
       driverValues = [
